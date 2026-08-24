5a9008658d13ad237b96734251ca875d380956a5
Mohamed Bashircls <mohamedbashirnuh@gmail.com>
2026-08-24 21:21:43 +0300

    protect /faculty_user in middleware

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 export { auth as proxy } from "@/lib/backend_super_admin/auth/auth"
 
 export const config = {
-  matcher: ["/super_admin/:path*"],
+  matcher: ["/super_admin/:path*", "/faculty_user/:path*"],
 }
