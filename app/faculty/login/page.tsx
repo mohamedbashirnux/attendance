6c4da214c863afa0ca46a261adf405283e0dd6d6
Mohamed Bashircls <mohamedbashirnuh@gmail.com>
2026-09-11 20:59:59 +0300

    updated ui

--- app/faculty/login/page.tsx
+++ app/faculty/login/page.tsx
@@ -4,7 +4,7 @@ import { FacultyLoginForm } from "./_components/faculty-login-form"
 export default async function Page() {
   const faculties = await getFaculties()
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center bg-[#fafafa] p-6 md:p-10">
       <div className="w-full max-w-sm">
         <FacultyLoginForm faculties={faculties} />
       </div>
